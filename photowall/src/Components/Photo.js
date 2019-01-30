c1bfaf06dcbe3431dccaf303948cfa47d71ca70a
mckornfield <matthew.kornfield@appian.com>
2019-01-30 16:34:45 -0500

    stateless functional components

--- photowall/src/Components/Photo.js
+++ photowall/src/Components/Photo.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Photo extends Component {
-    render() {
-        const post = this.props.post;
-        return <figure class="figure">
-            <img class="photo" src={post.imageLink} alt={post.description}></img>
-            <figcaption> <p>{post.description}</p></figcaption>
-            <div class="button-container">
-                <button class="remove-button"> Remove </button>
-            </div>
-        </figure>
-    }
+function Photo(props) {
+    const post = props.post;
+    return <figure class="figure">
+        <img class="photo" src={post.imageLink} alt={post.description}></img>
+        <figcaption> <p>{post.description}</p></figcaption>
+        <div class="button-container">
+            <button class="remove-button"> Remove </button>
+        </div>
+    </figure>
 }
 
 export default Photo;
