9c1cdf823382e4a32306903e3aa1c759717b0f9b
mckornfield <matthew.kornfield@appian.com>
2019-01-30 17:18:45 -0500

    proptypes

--- photowall/src/Components/Photo.js
+++ photowall/src/Components/Photo.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function Photo(props) {
     const post = props.post;
-    return <figure class="figure">
-        <img class="photo" src={post.imageLink} alt={post.description}></img>
+    return <figure className="figure">
+        <img className="photo" src={post.imageLink} alt={post.description}></img>
         <figcaption> <p>{post.description}</p></figcaption>
-        <div class="button-container">
-            <button class="remove-button"
+        <div className="button-container">
+            <button className="remove-button"
                 onClick={() => props.onRemovePhoto(post)}> Remove </button>
         </div>
     </figure>
 }
 
+Photo.propTypes = {
+    post: PropTypes.object.isRequired,
+    onRemovePhoto: PropTypes.func.isRequired
+}
 export default Photo;
