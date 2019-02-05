78ff824440de4207af505350c43dcb79408d8d1d
Matthew Kornfield <matthew.kornfield@walmart.com>
2019-02-05 10:04:04 -0500

    initial redux files

--- photowall/src/index.js
+++ photowall/src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './Components/Main';
 import './styles/stylesheet.css';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import rootReducer from './redux/reducer.js'
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(<BrowserRouter><Main /></BrowserRouter>, document.getElementById('root'));
---
+++ photowall/src/redux/reducer.js
@@ -0,0 +1,7 @@
+import posts2 from '../data/posts'
+
+const postReducer = function posts(state = posts2, action) {
+    return state;
+}
+
+export default postReducer;
