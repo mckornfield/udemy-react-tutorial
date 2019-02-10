215979dc5f71e75f8d874e360d6a9dfb7ab57c52
mckornfield <matthew.kornfield@appian.com>
2019-02-10 12:46:48 -0500

    added addphoto action back

--- photowall/src/Components/Main.js
+++ photowall/src/Components/Main.js
@@ -2,25 +2,23 @@ import React, { Component } from 'react';
 import Title from './Title'
 import PhotoWall from './PhotoWall';
 import AddPhoto from './AddPhoto';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 class Main extends Component {
 
     render() {
-        console.log(this.props);
         return (<div>
+            <h1>
+                <Link to="/">Photowall</Link>
+            </h1>
             <Route exact path="/" render={() => {
                 return <div>
-                    <Title title={'Photowall'} />
                     <PhotoWall {...this.props} />
                 </div>
             }} />
 
             <Route path="/AddPhoto" render={({ history }) => {
-                return <AddPhoto onAddPhoto={(addedPhoto) => {
-                    history.push('.');
-            }
-                } />
+                return <AddPhoto {...this.props} />
             }} />
         </div>
         )
--- photowall/src/redux/actions.js
+++ photowall/src/redux/actions.js
@@ -8,3 +8,10 @@ export function removePost(index) {
 }
 
 // function addPost(index)
+
+export function addPost(post) {
+    return {
+        type: "ADD_POST",
+        post
+    };
+}
